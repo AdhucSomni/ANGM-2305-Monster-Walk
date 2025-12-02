745847911f701c9191d2b1c6dbfe6a34469853a7
Lazarus Avila <lazarusavila15@gmail.com>
2025-12-01 19:21:34 -0600

    using os and variables to locate the project folders and the folders containing the sprites

--- src/player.py
+++ src/player.py
@@ -4,3 +4,9 @@ import os
 class Player(pygame.sprite.Sprite):
 
     def __init__(self, start_pos):
+        super().__init__()
+
+        #find the project on disk
+        project_folder = os.path.dirname(__file__)
+        #find the sprites
+        self.sprite_folder = os.path.join(project_folder, "Sprite pngs")
