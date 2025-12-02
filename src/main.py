ebc065d7f4951819c76e14c6dbe7e13bd40af7b7
Lazarus Avila <lazarusavila15@gmail.com>
2025-12-01 20:31:42 -0600

    added code to def main() in main.py to create a window at full screen resolution

--- src/main.py
+++ src/main.py
@@ -4,3 +4,9 @@ from player import Player
 
 def main():
     pygame.init()
+
+    display = pygame.display.Info()
+    screen_width, screen_height = display.current_w, display.current_h
+
+    screen = pygame.display.set_mode((screen_width, screen_height), pygame.FULLSCREEN)
+    
