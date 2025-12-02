0a7d9f6c7a931327f2b53a997a17aac2013656af
Lazarus Avila <lazarusavila15@gmail.com>
2025-12-01 21:23:20 -0600

    bug fixing main.py

--- src/main.py
+++ src/main.py
@@ -45,24 +45,24 @@ def main():
                 elif event.key == pygame.K_d:
                     player.move_right = False
 
-    player.update(dt, screen_width)
+        player.update(dt, screen_width)
 
-    screen.fill(sky_color)
+        screen.fill(sky_color)
 
-    ground = pygame.Rect(
-        0,
-        screen_height - ground_height,
-        screen_width,
-        ground_height
-    )
-    pygame.draw.rect(screen, ground_color, ground)
+        ground = pygame.Rect(
+            0,
+            screen_height - ground_height,
+            screen_width,
+            ground_height
+        )
+        pygame.draw.rect(screen, ground_color, ground)
 
-    player.draw(screen)
+        player.draw(screen)
 
-    pygame.display.flip()
+        pygame.display.flip()
 
-pygame.quit()
-sys.exit()
+    pygame.quit()
+    sys.exit()
 
 if __name__ == "__main__":
     main()
