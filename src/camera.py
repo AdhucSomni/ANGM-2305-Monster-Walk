30e3f24880fc6e237b9829cce932f3ada91ffe61
Lazarus Avila <lazarusavila15@gmail.com>
2025-12-04 17:26:47 -0600

    completed initialize funcion and creating the follow function in the Camera class

--- src/camera.py
+++ src/camera.py
@@ -2,3 +2,7 @@ class Camera:
     """Horizontal camera that follows the sprite along the x-axis"""
 
     def __init__(self, screen_width: int):
+        self.screen_width = screen_width
+        self.offset_x = 0
+
+    def follow(self, target_rect):
