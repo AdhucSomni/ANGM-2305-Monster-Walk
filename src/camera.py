a8e2318c162732a10d21c8eccb595eed6e21a68f
Lazarus Avila <lazarusavila15@gmail.com>
2025-12-08 07:37:54 -0600

    cleaning up files/mistakes

--- src/camera.py
+++
@@ -1,9 +0,0 @@
-class Camera:
-    """Horizontal camera that follows the sprite along the x-axis"""
-
-    def __init__(self, screen_width: int):
-        self.screen_width = screen_width
-        self.offset_x = 0
-
-    def follow(self, target_rect):
-        self.offset_x = target_rect.centerx - self.screen_width // 2
